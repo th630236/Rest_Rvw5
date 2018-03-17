92a55ba2b7e121187d21a9f6682cd0e1cddeb016
Timothy M Hitchcock <timh412@inebraska.com>
2018-03-17 02:45:29 -0500

    Addition of Role Code (not on menu yet) Movement of 'Update Acct Info' menu itm into dropdown

---
+++ Rest_Rvw5/Models/ApplicationRole.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+
+namespace Rest_Rvw5.Models
+{
+    public class ApplicationRole : IdentityRole
+    {
+
+    }
+}
